14a4942a75b2ca82e38859a58794005d5a01c124
s3xmaxman <badside3919@gmail.com>
2024-12-04 05:14:28 +0900

    build(deps): update package-lock.json, package.json, App.jsx and tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Hello World</h1>
+    <main className="relative min-h-screen w-screen overflow-hidden">
+      <Hero />
     </main>
   );
 };
