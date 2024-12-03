6ef5534a705f7640deba6cde819ec158335a90de
s3xmaxman <badside3919@gmail.com>
2024-12-04 03:20:19 +0900

    update 4 files and delete 1 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">Hello World</h1>
+    </main>
+  );
 };
 
 export default App;
