b646c40ca8a732468de88b5d2bc6cbe33f20d122
s3xmaxman <badside3919@gmail.com>
2024-12-05 07:37:50 +0900

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
 
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
+      <Features />
+      <Story />
     </main>
   );
 };
